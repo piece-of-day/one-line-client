763ea8c00e01d2d85f0aba341d1ea1f3c0a65183
LeeMir <manofgodmj@gmail.com>
2022-12-08 16:46:42 +0900

    [Design] global style 설정

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,12 +2,19 @@ import reset from 'styled-reset';
 import { createGlobalStyle } from 'styled-components';
 
 import { ColorCode } from '@/constants/color';
+import BackgroundImg from '@/assets/images/background.png';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
   html {
     font-size: calc(min(0.35vw, 3.76px) + 8.74px);
     background-color: ${ColorCode.OFFWHITE};
+
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+    &::-webkit-scrollbar {
+      display: none; /* Chrome, Safari, Opera*/
+    }
   }
   html,
   body,
@@ -18,6 +25,18 @@ const GlobalStyle = createGlobalStyle`
   textarea,
   button {
     font-family: 'NotoSansKR', sans-serif;
+  }
+
+  button {
+    background: none;
+    outline: none;
+    border: none;
+  }
+
+  #root {
+    width: 100vw;
+    background-image: url('${BackgroundImg}');
+    background-attachment: fixed;
   }
 `;
 
