70418dd7196027b3f4a70c32997f38208e280594
LeeMir <manofgodmj@gmail.com>
2023-01-20 02:11:23 +0900

    [Chore] MainPageStateValue 주석 작성
    적절한 이름을 짓기 어려움

--- src/types/state.ts
+++ src/types/state.ts
@@ -3,6 +3,6 @@ import { ValueOf } from '@/utils/helperType';
 import { PAGE_STATE } from '@/constants/state';
 
 export interface MainPageStateValue {
-  pageState: ValueOf<typeof PAGE_STATE>;
-  selectedMsgIdx: number;
+  pageState: ValueOf<typeof PAGE_STATE>; // 페이지 상태
+  selectedMsgIdx: number; // 선택한 한 줄 id
 }
