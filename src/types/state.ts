11e9a7001167dc3855d024be55c94fdd4efd2d82
LeeMir <manofgodmj@gmail.com>
2023-03-28 17:15:19 +0900

    [Refactor] 메시지 idx => id로 변경

--- src/types/state.ts
+++ src/types/state.ts
@@ -4,5 +4,5 @@ import { PAGE_STATE } from '@/constants/state';
 
 export interface MainPageStateValue {
   pageState: ValueOf<typeof PAGE_STATE>; // 페이지 상태
-  selectedMsgIdx: number; // 선택한 한 줄 id
+  selectedMsgId: number; // 선택한 한 줄 id
 }
